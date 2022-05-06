dc933461a26719ac8403c69bfdbd95af8f04576a
Neil Baker <Neil.Baker2@atkinsglobal.com>
2022-05-06 10:38:12 +0100

    Add project files.

---
+++ Configuration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IdentityModel;
+using IdentityServer4.Models;
+
+namespace IdentityServer
+{
+    public static class Configuration
+    {
+        public static IEnumerable<ApiResource> GetApis() =>
+            new List<ApiResource> {
+                new ApiResource("WebPortal")
+            };
+
+        public static IEnumerable<Client> GetClients() =>
+            new List<Client> { 
+                new Client {
+                    ClientId = "client_id",
+                    ClientSecrets = {new Secret("client_secret".ToSha256())},
+
+                }
+            };
+    }
+}
